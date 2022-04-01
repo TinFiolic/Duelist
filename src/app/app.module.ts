17c41c5285ce2b53c195b00eb44a30df18c31684
TinFiolic <75586429+TinFiolic@users.noreply.github.com>
2022-04-01 20:50:38 +0200

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { AngMusicPlayerModule } from  'ang-music-player';
 import { CommonModule } from  '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AlbumComponent } from './album/album.component';
+import { StreamingComponent } from './streaming/streaming.component';
+import { DownloadComponent } from './download/download.component';
 import { MainComponent } from './main/main.component';
 import { IntroComponent } from './intro/intro.component';
 import { FooterComponent } from './footer/footer.component';
@@ -13,7 +15,9 @@ import { FooterComponent } from './footer/footer.component';
     AlbumComponent,
     MainComponent,
     IntroComponent,
-    FooterComponent
+    FooterComponent,
+    StreamingComponent,
+    DownloadComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
